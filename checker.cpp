49f4587dbd5517382462740cfd84792ed7e34d52
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-03-02 20:08:01 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 enum BMS_Parameters {temp,sate_of_charge,charge_rate};
-const char* BMS_Parameters_Name = { "Temperature" ,"SOC" , "ChargeRate" };
+const char* BMS_Parameters_Name[] = { "Temperature" ,"SOC" , "ChargeRate" };
 class BMS{
   private:
     bool BMS_OK;
